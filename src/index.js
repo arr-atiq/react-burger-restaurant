1c2eb28757f68135a93cdd1596fe9ff77e51a728
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-01 02:03:00 +0600

    update project folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.mis.css';
 
 ReactDOM.render(
   <React.StrictMode>
